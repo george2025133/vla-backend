d957862e7bde55bb36ef2778e2d2e95e90bea9f5
george2025133 <jcarvallo@vlacademy.com>
2025-07-01 19:41:30 -0600

    upload-files

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+const port = 5000; 
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'jorge133',
+  database: 'dbnode'
+});
+
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Conexión a la base de datos MySQL establecida');
+});
+
+
+app.get('/api/data', (req, res) => {
+  const sql = 'SELECT * FROM customer';
+  db.query(sql, (err, results) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.json(results);
+  });
+});
+
+
+app.post('/api/customers', (req, res) => {
+  const newCustomer = req.body;
+  const sql = 'INSERT INTO customer (name, address, phone) VALUES (?, ?, ?)';
+  db.query(sql, [newCustomer.name, newCustomer.address, newCustomer.phone], (err, result) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.json({ id: result.insertId, ...newCustomer });
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`Servidor backend escuchando en http://localhost:${port}`);
+});
